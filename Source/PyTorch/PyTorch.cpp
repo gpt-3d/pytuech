909c9a69d1c693a7cc38651ed1cf380cb22dad0b
db <jdebelec@hotmail.com>
2024-04-03 16:19:31 +1100

    Initial commit

---
+++ Source/PyTorch/PyTorch.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PyTorch.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PyTorch, "PyTorch" );
